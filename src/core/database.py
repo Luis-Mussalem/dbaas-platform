89de3f4132552e57669c851218d999f895c9b95c
Luis Mussalem <luismussalem@gmail.com>
2026-04-07 18:08:26 -0300

    feat: add SQLAlchemy sync engine, session factory and Base

---
+++ src/core/database.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, Session, sessionmaker
+
+from src.core.config import settings
+
+engine = create_engine(settings.DATABASE_URL, echo=settings.DEBUG)
+
+SessionLocal = sessionmaker(bind=engine, class_=Session, expire_on_commit=False)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
