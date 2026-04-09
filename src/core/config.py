ef10cb443171add58c9d4d0e7024cf3fc77cb597
Luis Mussalem <luismussalem@gmail.com>
2026-04-09 15:06:54 -0300

    feat: rebrand to PalmTreeDB with secure credential placeholders

--- src/core/config.py
+++ src/core/config.py
@@ -9,14 +9,14 @@ class Settings(BaseSettings):
     )
 
     # Application
-    APP_NAME: str = "DBaaS Platform"
+    APP_NAME: str = "PalmTreeDB"
     APP_VERSION: str = "0.1.0"
     DEBUG: bool = False
 
     # Database
-    POSTGRES_USER: str = "dbaas"
-    POSTGRES_PASSWORD: str = "dbaas_secret"
-    POSTGRES_DB: str = "dbaas"
+    POSTGRES_USER: str = "palmtreedb"
+    POSTGRES_PASSWORD: str = "change-me"
+    POSTGRES_DB: str = "palmtreedb"
     POSTGRES_HOST: str = "localhost"
     POSTGRES_PORT: int = 5432
 
